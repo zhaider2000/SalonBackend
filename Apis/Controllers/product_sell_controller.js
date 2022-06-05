bf32f9deb5bbe9d8ff9f361f5f4e610ad3ca5cc4
Mubashir Ansari <mubassheransari@gmail.com>
2022-06-06 02:55:53 +0500

    deployed link

--- Apis/Controllers/product_sell_controller.js
+++ Apis/Controllers/product_sell_controller.js
@@ -1,75 +1,64 @@
-'use strict'
-const sellProductService=require('../Services/product_sell_service');
+"use strict";
+const sellProductService = require("../Services/product_sell_service");
 
+module.exports = class AvailedServic {
+  static async sellProdcut(req, res, next) {
+    try {
+      console.log("here at sell product");
+      const { salon, user, products, quantities } = req.body;
+      const email = req.body.email;
+      console.log(email);
 
+      let newSoldProdcut = await sellProductService.soldProduct(req.body);
 
+      if (newSoldProdcut == true) {
+        console.log(salon, user, products, quantities);
+        res.json({ messsage: "sucess" });
+        await sendProductEmail(req.body.email);
+      }
 
-module.exports=class AvailedServic{
-
-    static async sellProdcut(req,res,next){
-
-        try {
-
-            console.log("here at sell product");
-
-            let newSoldProdcut=await sellProductService.soldProduct(req.body)
-
-            if(newSoldProdcut==true){
-                res.json({messsage:"sucess"})
-            }
-
-            if(newSoldProdcut==false){
-
-                res.json({messsage:"fail"})
-            }
-
-            
-        } catch (error) {
-
-            console.log(error)
-        }
-
+      if (newSoldProdcut == false) {
+        res.json({ messsage: "fail" });
+        await sendProductEmail(email, "123");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
+  static async getUserAvailedServices(req, res, nex) {
+    try {
+      let userServices = await availedService.getUserAvailedServices(
+        req.body.id
+      );
 
-    static async getUserAvailedServices(req,res,nex){
+      if (userServices == false) {
+        res.json({ messsage: "no availed services / error" });
+      }
 
-        try {
-
-            let userServices=await availedService.getUserAvailedServices(req.body.id)
-
-            if(userServices==false){
-                res.json({messsage:"no availed services / error"})
-            }
-            
-            if(userServices){
-                res.json(userServices)
-            }
-            
-        } catch (error) {
-                console.log(error)
-        }
+      if (userServices) {
+        res.json(userServices);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    static async getSalonAvailedServices(req,res,nex){
+  static async getSalonAvailedServices(req, res, nex) {
+    try {
+      let salonServices = await availedService.getSalonAvailedServices(
+        req.body.id
+      );
 
-        try {
+      if (salonServices == false) {
+        res.json({ messsage: "no availed services / error" });
+      }
 
-            let salonServices=await availedService.getSalonAvailedServices(req.body.id)
-
-            if(salonServices==false){
-                res.json({messsage:"no availed services / error"})
-            }
-            
-            if(salonServices){
-                res.json(salonServices)
-            }
-            
-        } catch (error) {
-                console.log(error)
-        }
+      if (salonServices) {
+        res.json(salonServices);
+      }
+    } catch (error) {
+      console.log(error);
     }
-
-
-
-}
+  }
+};
